791d785ebd174e3bb220d4a7fd82e92b4e0f0011
Fanaar <fanaarrashid@gmail.com>
2025-12-15 19:44:14 +0100

    Rabbit hole blockade mechanic werkt

--- Over the Log/Assets/Fanaar/Scripts/Rabbit/RabbitSettlementManager.cs
+++ Over the Log/Assets/Fanaar/Scripts/Rabbit/RabbitSettlementManager.cs
@@ -1,32 +1,72 @@
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class RabbitSettlementManager : MonoBehaviour
 {
-    [Header("Hond Settings")]
-    public DogController2 dog;
-    public int rabbitsToSettleForDog = 3; // aantal konijnen voordat hond verschijnt
+    public static RabbitSettlementManager Instance;
 
+    [Header("Rabbits")]
+    public int totalRabbits = 5;                        // Totaal aantal konijnen
     private HashSet<RabbitRunAwayController> settledRabbits = new HashSet<RabbitRunAwayController>();
-    private bool dogActivated = false;
 
-    // Wordt door elk konijn aangeroepen
+    [Header("Burrow / Escape")]
+    public float digTime = 30f;                         // Tijd voordat burrow open is
+    public GameObject[] blockingCubes;                  // Cubes die de opening blokkeren
+    public AudioSource diggingAudio;                    // Optionele audio
+    public ParticleSystem diggingParticles;            // Optionele particle effect
+
+    [Header("Dog")]
+    public GameObject dog;                              // Sleep hier de hond in inspector
+    private bool diggingStarted = false;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    /// <summary>
+    /// Wordt aangeroepen door konijn wanneer het gesettled is
+    /// </summary>
     public void RabbitSettled(RabbitRunAwayController rabbit)
     {
         if (settledRabbits.Contains(rabbit))
-            return; // voorkom dubbel tellen
+            return;
 
         settledRabbits.Add(rabbit);
-        Debug.Log("Konijnen gesettled: " + settledRabbits.Count);
+        Debug.Log($"Konijn gesettled! Totaal: {settledRabbits.Count}/{totalRabbits}");
 
-        if (!dogActivated && settledRabbits.Count >= rabbitsToSettleForDog)
+        // Hond activeren bij het 3e konijn
+        if (settledRabbits.Count == 3 && dog != null)
         {
-            if (dog != null)
-            {
-                dog.gameObject.SetActive(true);
-                dogActivated = true;
-                Debug.Log("Hond geactiveerd!");
-            }
+            dog.SetActive(true);
+            Debug.Log("Hond wordt geactiveerd bij 3e konijn!");
         }
+
+        // Start burrow/graven timer pas als alle konijnen gesettled zijn
+        if (settledRabbits.Count >= totalRabbits && !diggingStarted)
+        {
+            StartCoroutine(DigBurrow());
+        }
+    }
+
+    private IEnumerator DigBurrow()
+    {
+        diggingStarted = true;
+        Debug.Log("Burrow graven gestart!");
+
+        if (diggingAudio) diggingAudio.Play();
+        if (diggingParticles) diggingParticles.Play();
+
+        yield return new WaitForSeconds(digTime);
+
+        foreach (GameObject cube in blockingCubes)
+        {
+            cube.SetActive(false);
+        }
+
+        Debug.Log("Burrow is nu open!");
+        if (diggingAudio) diggingAudio.Stop();
+        if (diggingParticles) diggingParticles.Stop();
     }
 }
